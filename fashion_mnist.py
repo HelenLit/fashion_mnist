832bc823441f4434a226915f0b87a3cfea0afc95
Olena Litovska <litovskaolena@gmail.com>
2023-08-08 18:27:17 +0300

    This is the same neural network as earlier, but this time with Convolution and MaxPooling layers added first

--- fashion_mnist.py
+++ fashion_mnist.py
@@ -38,25 +38,68 @@ training_images  = training_images / 255.0
 test_images = test_images / 255.0
 
 
-# Build the classification model
-model = tf.keras.models.Sequential([tf.keras.layers.Flatten(input_shape=(28, 28)),
-                                    tf.keras.layers.Dense(512, activation=tf.nn.relu),
-                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+# Define the model
+model = tf.keras.models.Sequential([
+                                                         
+  # Add convolutions and max pooling
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+  tf.keras.layers.MaxPooling2D(2,2),
 
-model.compile(optimizer = tf.optimizers.Adam(),
-              loss = 'sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+  # Add the same layers as before
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
+])
 
-model.fit(training_images, training_labels, epochs=10, callbacks=myCallback)
-#Usually 2 epoch is enough
+# Print the model summary
+model.summary()
 
-# Evaluate the model on unseen data
-model.evaluate(test_images, test_labels)
+# Use same settings
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-classifications = model.predict(test_images)
+# Train the model
+print(f'\nMODEL TRAINING:')
+model.fit(training_images, training_labels, epochs=5)
 
-print(classifications[0])
+# Evaluate on the test set
+print(f'\nMODEL EVALUATION:')
+test_loss = model.evaluate(test_images, test_labels)
 
-print(test_labels[0])
+import matplotlib.pyplot as plt
+from tensorflow.keras import models
 
+f, axarr = plt.subplots(3,4)
 
+FIRST_IMAGE=0
+SECOND_IMAGE=23
+THIRD_IMAGE=28
+CONVOLUTION_NUMBER = 0
+
+layer_outputs = [layer.output for layer in model.layers]
+activation_model = tf.keras.models.Model(inputs = model.input, outputs = layer_outputs)
+
+for x in range(0,4):
+  f1 = activation_model.predict(test_images[FIRST_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[0,x].imshow(f1[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[0,x].grid(False)
+  
+  f2 = activation_model.predict(test_images[SECOND_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[1,x].imshow(f2[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[1,x].grid(False)
+  
+  f3 = activation_model.predict(test_images[THIRD_IMAGE].reshape(1, 28, 28, 1))[x]
+  axarr[2,x].imshow(f3[0, : , :, CONVOLUTION_NUMBER], cmap='inferno')
+  axarr[2,x].grid(False)
+
+#classifications = model.predict(test_images)
+
+#print(classifications[0])
+
+#print(test_labels[0])
+
+#
+#MODEL TRAINING - loss: 0.2224 - accuracy: 0.9180
+#MODEL EVALUATION - loss: 0.2668 - accuracy: 0.9011
+#
